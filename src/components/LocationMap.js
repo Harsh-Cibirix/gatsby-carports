6d5743d945acfd96171fd85ac0b4bc81d87de4ad
Harsh-Cibirix <56672005+Harsh-Cibirix@users.noreply.github.com>
2019-11-28 17:36:10 +0530

    Fixed the mapbox issue during deployment

--- src/components/LocationMap.js
+++ src/components/LocationMap.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react"
-import ReactMapboxGl, {Layer, Feature, Map} from "react-mapbox-gl"
+// import ReactMapboxGl, {Layer, Feature, Map} from "react-mapbox-gl"
 
 class LocationMap extends Component{
-    Map = ReactMapboxGl({
-        accessToken:
-          'undefined'
-      });
+    // Map = ReactMapboxGl({
+    //     accessToken:
+    //       'undefined'
+    //   });
 
     render(){
         return(
@@ -15,11 +15,11 @@ class LocationMap extends Component{
                     <div class="col-12">
                         <h4 class="section__title map-section__title">We Got you covered in these Four states</h4>
                         <div id="map" class="map map-section__grid">
-                        <Map className="map map-section__grid" style="mapbox://styles/mapbox/streets-v9"> 
+                        {/* <Map className="map map-section__grid" style="mapbox://styles/mapbox/streets-v9"> 
                             <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
                                 <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
                             </Layer>
-                        </Map>
+                        </Map> */}
                     </div>
                     </div>
                 </div>
